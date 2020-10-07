9f176eeb29f1e3adb2a8b0043b4ee2cd807526f5
PatrickJDuffy <PatrickJDuffy@zoho.eu>
2020-10-07 13:50:30 +0100

    ...

--- src/main/java/com/mycompany/softwareengineering/Course.java
+++ src/main/java/com/mycompany/softwareengineering/Course.java
@@ -6,12 +6,12 @@ public class Course {
 private String name;
 private Student students[];
 private Module modules[];
-private DateTime startDate, endDate;
+//private DateTime startDate, endDate;
 
-public Course(String Name, DateTime startDate, DateTime endDate){
+public Course(String Name/*, DateTime startDate, DateTime endDate*/){
 this.name = name;
-this.startDate= startDate;
-this.endDate = endDate;
+//this.startDate= startDate;
+//this.endDate = endDate;
 }
 
 //Add addStudent, addModule, 
